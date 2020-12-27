0342fe8c9a1c61fb4a6167128a0aef810da355b2
Alexander Leon <1995.Alexander.Leon@gmail.com>
2020-12-26 19:31:57 -0800

    env variable issue attempted fix

--- src/client/App.js
+++ src/client/App.js
@@ -32,7 +32,7 @@ class App extends React.Component{
 
 
   loadSheet = async() => {
-    const {data} = await axios(SHEET_URL)
+    const {data} = await axios(process.env.SHEET_URL)
     console.log(data)
     return data
   }
